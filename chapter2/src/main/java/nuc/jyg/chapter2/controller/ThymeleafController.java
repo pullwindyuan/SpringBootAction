c35db2652a61536340d1e9aeb677ab0f84cd4d89
jiyongguang <jiyongguang0723@gmail.com>
2017-09-08 21:30:17 +0800

    SpringBoot + Thymeleaf

--- chapter2/src/main/java/nuc/jyg/chapter2/controller/ThymeleafController.java
+++ chapter2/src/main/java/nuc/jyg/chapter2/controller/ThymeleafController.java
@@ -1,7 +1,9 @@
 package nuc.jyg.chapter2.controller;
 
+import nuc.jyg.chapter2.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
 
-    @RequestMapping(path = {"/interview"}, method = {RequestMethod.GET})
+    @RequestMapping(path = "/interview", method = {RequestMethod.GET})
     public String interview(@RequestParam(name = "name", required = false, defaultValue = "interview") String name,
                             Model model) {
-
-        model.addAttribute("templateName",name);
+        model.addAttribute("templateName", name);
         return "interview";
+    }
+
+    @RequestMapping(value = "/visityuser", method = RequestMethod.GET)
+    public String visityUser(ModelMap modelMap) {
+        User user = new User();
+        user.setUsername("jyg");
+        user.setPassword("jyg");
+        modelMap.addAttribute("user", user);
+        return "visityuser";
     }
 }
---
+++ chapter2/src/main/java/nuc/jyg/chapter2/model/User.java
@@ -0,0 +1,28 @@
+package nuc.jyg.chapter2.model;
+
+/**
+ * @Author Nuc YongGuang Ji.
+ * @Date 21:24 2017/9/8.
+ * @Descrip
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
