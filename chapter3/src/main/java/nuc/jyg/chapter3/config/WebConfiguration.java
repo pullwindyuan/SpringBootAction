c2418916d34295f060a8576f8f2cbd5afd9d1bc0
jiyongguang <jiyongguang0723@gmail.com>
2017-09-08 21:37:43 +0800

    SpringBoot Configuration Filter

---
+++ chapter3/src/main/java/nuc/jyg/chapter3/config/WebConfiguration.java
@@ -0,0 +1,53 @@
+package nuc.jyg.chapter3.config;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        // 如果我们的应用程序运行在一台负载均衡代理服务器后方，
+        // 因此需要将代理服务器发来的请求包含的IP地址转换成真正的用户IP。
+        return new RemoteIpFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        // 可以详细地更好地详细的定义过滤器
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    public class MyFilter implements Filter {
+
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+
+        }
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            System.out.println("this is MyFilter,url:" + request.getRequestURI());
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+        @Override
+        public void destroy() {
+
+        }
+    }
+}
